31dde8bb7b3d194235e3af0950978f964aebd2c4
Nar <51009725+narvmtz@users.noreply.github.com>
2020-04-21 17:27:56 -0500

    Updates

---
+++ main.js
@@ -0,0 +1,30 @@
+const gigiBirthday = new Date("Apr 22, 2020 00:00:00").getTime();
+
+// Update the count down every 1 second
+let timer = setInterval(function () {
+  // Get todays date and time
+  const now = new Date().getTime();
+
+  // Find the distance between now and the countdown date
+  const distanceBday = gigiBirthday - now;
+
+  // Time calculations for  hours, minutes and seconds
+  //const daysB = Math.floor(distanceBday / (1000 * 60 * 60 * 24));  *days
+  const hoursBday = Math.floor(
+    (distanceBday % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutesBday = Math.floor((distanceBday % (1000 * 60 * 60)) / (1000 * 60));
+  const secondsBday = Math.floor((distanceBday % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("gigiDay").innerHTML =
+    hoursBday + "h " + minutesBday + "m " + secondsBday + "s ";
+
+  // If the count down is finished, write some text
+  if (distanceBday <= 0) {
+    clearInterval(timer);
+    document.getElementById("title").innerHTML = "🎉Today is the big day!🎉";
+    document.getElementById("gigiDay").innerHTML = "🎊Happy Birthday Gigi!🎊 🎁";
+  }
+}, 1000);
+
