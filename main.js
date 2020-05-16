8047e4af414016aec0ae5eb6a7d3aea8094d4f8a
Nar <51009725+narvmtz@users.noreply.github.com>
2020-05-16 00:07:20 -0500

    Update design and add image

--- main.js
+++ main.js
@@ -1,36 +1,46 @@
-const countdownForUs = new Date("Mar 15, 2021 00:00:00").getTime();
+const countdown = new Date("Mar 15, 2021 00:00:00").getTime();
 
-// Update the count down every 1 second
+// Update the count down every second
 let timer = setInterval(() => {
   // Get todays date and time
   const now = new Date().getTime();
 
   // Find the distance between now and the countdown date
-  const distanceBday = countdownForUs - now;
+  const distanceDay = countdown - now;  
 
   // Time calculations for  hours, minutes and seconds
   const daysDay = Math.floor(
-    distanceBday / (1000 * 60 * 60 * 24)
+    distanceDay / (1000 * 60 * 60 * 24)
   );
   const hoursDay = Math.floor(
-    (distanceBday % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    (distanceDay % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
   );
   const minutesDay = Math.floor(
-    (distanceBday % (1000 * 60 * 60)) / (1000 * 60)
+    (distanceDay % (1000 * 60 * 60)) / (1000 * 60)
   );
   const secondsDay = Math.floor(
-    (distanceBday % (1000 * 60)) / 1000
+    (distanceDay % (1000 * 60)) / 1000
   );
 
-  // Display the result in the element with id="demo"
-  document.getElementById("countdown").innerHTML =
-    (`${daysDay} D ${hoursDay} h ${minutesDay} m ${secondsDay} s`);
+  // Storage the variables to show the countdown
+  const day = `${daysDay}`;
+  const hour = `${hoursDay}`;
+  const min = `${minutesDay}`;
+  const sec = `${secondsDay}`;
 
   // If the count down is finished, write some text
-  if (distanceBday <= 0) {
+  if (distanceDay <= 0) {
     clearInterval(timer);
     document.getElementById("title").innerHTML = "🎉Today is the big day!🎉";
-    document.getElementById("countdown").innerHTML = "🎊Happy!🎊 🎁";
+    document.getElementById("firstPara").innerHTML = "Let's celebrate";
+    document.getElementById("secondPara").innerHTML = "🎁 🎊 We're together now! 🎊 🎁"
+  }
+  //Else show the countdown
+  else { 
+    document.getElementById('day').innerHTML = day;
+    document.getElementById('hour').innerHTML = hour;
+    document.getElementById('min').innerHTML = min;
+    document.getElementById('sec').innerHTML = sec;
   }
 }, 1000);
 
